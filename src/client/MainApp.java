aeaf59e82f6be5a0447508a1c568fa7f351c990f
Nicolaj Lundtoft Jørgensen <hapse@hh4.dk>
2017-09-27 18:32:58 +0200

    Spawner nu personer

--- src/client/MainApp.java
+++ src/client/MainApp.java
@@ -21,110 +21,131 @@ import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
-	public static final int size = 20;
-	public static final int scene_height = size * 20 + 100;
-	public static final int scene_width = size * 20 + 200;
+    public static final int size = 20;
+    public static final int scene_height = size * 20 + 100;
+    public static final int scene_width = size * 20 + 200;
 
-	private Image image_floor;
-	private Image image_wall;
-	private Image hero_right, hero_left, hero_up, hero_down;
+    private Image image_floor;
+    private Image image_wall;
+    private Image hero_right, hero_left, hero_up, hero_down;
 
-	private String[] board;
+    private String[] board;
 
-	private Label[][] fields;
-	private TextArea scoreList;
+    private Label[][] fields;
+    private TextArea scoreList;
 
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			Socket clientSocket = new Socket("10.24.66.0", 1337);
-			DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-			BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			outToServer.writeBytes("NCasper\n");
-			board = updateBoard(inFromServer.readLine());
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Socket clientSocket = new Socket("192.168.2.29", 1337);
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            BufferedReader inFromServer =
+                new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            board = updateBoard(inFromServer.readLine());
 
-			GridPane grid = new GridPane();
-			grid.setHgap(10);
-			grid.setVgap(10);
-			grid.setPadding(new Insets(0, 10, 0, 10));
+            outToServer.writeBytes("NCasper\n");
+            
+            GridPane grid = new GridPane();
+            grid.setHgap(10);
+            grid.setVgap(10);
+            grid.setPadding(new Insets(0, 10, 0, 10));
 
-			Text mazeLabel = new Text("Maze:");
-			mazeLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+            Text mazeLabel = new Text("Maze:");
+            mazeLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20));
 
-			Text scoreLabel = new Text("Score:");
-			scoreLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+            Text scoreLabel = new Text("Score:");
+            scoreLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20));
 
-			scoreList = new TextArea();
+            scoreList = new TextArea();
 
-			GridPane boardGrid = new GridPane();
+            GridPane boardGrid = new GridPane();
 
-			image_wall = new Image(getClass().getResourceAsStream("Image/wall4.png"), size, size, false, false);
-			image_floor = new Image(getClass().getResourceAsStream("Image/floor1.png"), size, size, false, false);
+            image_wall = new Image(getClass().getResourceAsStream("Image/wall4.png"), size, size,
+                false, false);
+            image_floor = new Image(getClass().getResourceAsStream("Image/floor1.png"), size, size,
+                false, false);
 
-			hero_right = new Image(getClass().getResourceAsStream("Image/heroRight.png"), size, size, false, false);
-			hero_left = new Image(getClass().getResourceAsStream("Image/heroLeft.png"), size, size, false, false);
-			hero_up = new Image(getClass().getResourceAsStream("Image/heroUp.png"), size, size, false, false);
-			hero_down = new Image(getClass().getResourceAsStream("Image/heroDown.png"), size, size, false, false);
+            hero_right = new Image(getClass().getResourceAsStream("Image/heroRight.png"), size,
+                size, false, false);
+            hero_left = new Image(getClass().getResourceAsStream("Image/heroLeft.png"), size, size,
+                false, false);
+            hero_up = new Image(getClass().getResourceAsStream("Image/heroUp.png"), size, size,
+                false, false);
+            hero_down = new Image(getClass().getResourceAsStream("Image/heroDown.png"), size, size,
+                false, false);
 
-			fields = new Label[20][20];
-			for (int j = 0; j < 20; j++) {
-				for (int i = 0; i < 20; i++) {
-					switch (board[j].charAt(i)) {
-					case 'w':
-						fields[i][j] = new Label("", new ImageView(image_wall));
-						break;
-					case ' ':
-						fields[i][j] = new Label("", new ImageView(image_floor));
-						break;
-					default:
-						throw new Exception("Illegal field value: " + board[j].charAt(i));
-					}
-					boardGrid.add(fields[i][j], i, j);
-				}
-			}
-			scoreList.setEditable(false);
+            fields = new Label[20][20];
+            for (int j = 0; j < 20; j++) {
+                for (int i = 0; i < 20; i++) {
+                    switch (board[j].charAt(i)) {
+                    case 'w':
+                        fields[i][j] = new Label("", new ImageView(image_wall));
+                        break;
+                    case ' ':
+                        fields[i][j] = new Label("", new ImageView(image_floor));
+                        break;
+                    default:
+                        throw new Exception("Illegal field value: " + board[j].charAt(i));
+                    }
+                    boardGrid.add(fields[i][j], i, j);
+                }
+            }
+            scoreList.setEditable(false);
 
-			grid.add(mazeLabel, 0, 0);
-			grid.add(scoreLabel, 1, 0);
-			grid.add(boardGrid, 0, 1);
-			grid.add(scoreList, 1, 1);
+            grid.add(mazeLabel, 0, 0);
+            grid.add(scoreLabel, 1, 0);
+            grid.add(boardGrid, 0, 1);
+            grid.add(scoreList, 1, 1);
 
-			Scene scene = new Scene(grid, scene_width, scene_height);
-			primaryStage.setScene(scene);
-			primaryStage.show();
+            Scene scene = new Scene(grid, scene_width, scene_height);
+            primaryStage.setScene(scene);
+            primaryStage.show();
 
-			scene.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
-				switch (event.getCode()) {
-				case UP:
-					// playerMoved(0, -1, "up");
-					break;
-				case DOWN:
-					// playerMoved(0, +1, "down");
-					break;
-				case LEFT:
-					// playerMoved(-1, 0, "left");
-					break;
-				case RIGHT:
-					// playerMoved(+1, 0, "right");
-					break;
-				default:
-					break;
-				}
-			});
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            scene.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+                switch (event.getCode()) {
+                case UP:
+                    // playerMoved(0, -1, "up");
+                    break;
+                case DOWN:
+                    // playerMoved(0, +1, "down");
+                    break;
+                case LEFT:
+                    // playerMoved(-1, 0, "left");
+                    break;
+                case RIGHT:
+                    // playerMoved(+1, 0, "right");
+                    break;
+                default:
+                    break;
+                }
+            });
+            
+            //NAVN,X,Y,SCORE¤
+            
+            String[] ps = inFromServer.readLine().split("#");
 
-	public static String[] updateBoard(String s) {
-		String[] board = new String[(int) Math.sqrt(s.length())];
-		for (int i = 0; i < board.length; i++) {
-			board[i] = s.substring(i * board.length, (i + 1) * board.length);
-		}
-		return board;
-	}
+            for (String ss : ps) {
+                String[] s = ss.split(",");
+                System.out.println(s[1]);
+                System.out.println(s[2]);
+                fields[Integer.parseInt(s[1])][Integer.parseInt(s[2])]
+                    .setGraphic(new ImageView(hero_down));
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static String[] updateBoard(String s) {
+        String[] board = new String[(int) Math.sqrt(s.length())];
+        for (int i = 0; i < board.length; i++) {
+            board[i] = s.substring(i * board.length, (i + 1) * board.length);
+        }
+        return board;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
