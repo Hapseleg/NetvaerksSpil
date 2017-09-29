f40c20673c14a99e4a3dd537c261bffba3a69919
Casper Hedegaard <averagejoedk@gmail.com>
2017-09-29 11:15:18 +0200

    Platform.runLater

--- src/client/ReadThread.java
+++ src/client/ReadThread.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.Socket;
 
+import javafx.application.Platform;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
@@ -37,28 +38,32 @@ public class ReadThread extends Thread {
 				String input = inFromServer.readLine();
 				System.out.println(input);
 				String[] firstSplit = input.split("#");
-				scoreboard.setText("");
+				Platform.runLater(() -> {
+					scoreboard.setText("");
+				});
 				for (int i = 0; i < firstSplit.length; i++) {
 					String[] eachPlayer = firstSplit[i].split(",");
 					ImageView dir;
 					switch (eachPlayer[3]) {
-					case "up":
+					case "U":
 						dir = new ImageView(hero_up);
 						break;
-					case "down":
+					case "D":
 						dir = new ImageView(hero_down);
 						break;
-					case "left":
+					case "L":
 						dir = new ImageView(hero_left);
 						break;
-					case "right":
+					case "R":
 						dir = new ImageView(hero_right);
 						break;
 					default:
 						dir = new ImageView(hero_up);
 					}
-					scoreboard.setText(scoreboard.getText() + eachPlayer[0] + ": " + eachPlayer[4] + "\n");
-					fields[Integer.parseInt(eachPlayer[1])][Integer.parseInt(eachPlayer[2])].setGraphic(dir);
+					Platform.runLater(() -> {
+						scoreboard.setText(scoreboard.getText() + eachPlayer[0] + ": " + eachPlayer[4] + "\n");
+						fields[Integer.parseInt(eachPlayer[1])][Integer.parseInt(eachPlayer[2])].setGraphic(dir);
+					});
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
