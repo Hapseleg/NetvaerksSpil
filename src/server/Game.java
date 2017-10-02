30f72500a0defbf61995efc6bebf41a113a23e17
Nicolaj Lundtoft Jørgensen <hapse@hh4.dk>
2017-10-02 13:45:30 +0200

    protokol stuff

--- src/server/Game.java
+++ src/server/Game.java
@@ -12,7 +12,7 @@ public class Game {
     private Random rand;
     private int xSize, ySize;
     private ArrayList<Treasure> treasures;
-
+    
     public Game(String boardString) {
         this.players = new ArrayList<>();
         this.treasures = new ArrayList<>();
@@ -22,41 +22,41 @@ public class Game {
         this.ySize = 20;
         createBoardArray(xSize, ySize);
     }
-
+    
     private void createBoardArray(int x, int y) {
         boardArray = new String[x][y];
-        
+
         for (int i = 0; i < boardArray.length; i++) {
             boardArray[i] = board.substring(i * x, (i + 1) * y).split("(?!^)");
             System.out.println(Arrays.toString(boardArray[i]));
         }
     }
-    
+
 //    public ArrayList<PlayerThread> getPlayers() {
 //        return players;
 //    }
-
+    
     public void addPlayer(PlayerThread player) {
         try {
             spawnTreasure();//TODO bare for at teste spawn treasure
             addPlayerToBoard(player);
             this.players.add(player);
             player.sendMessage(board.toString());
-//            notifyPlayers();
+
         }
         catch (IOException e) {
             e.printStackTrace();
         }
     }
-
+    
     private synchronized void addPlayerToBoard(PlayerThread player) {
         boolean validPos = false;
         int x = 0, y = 0;
-        
+
         while (!validPos) {
             x = rand.nextInt(xSize - 1) + 1;
             y = rand.nextInt(xSize - 1) + 1;
-            
+
             if (!boardArray[y][x].equals("w")) {
                 boolean playerFoundAtPos = false;
                 int i = 0;
@@ -72,15 +72,15 @@ public class Game {
         player.setXpos(x);
         player.setYpos(y);
     }
-    
-    private void spawnTreasure() {
+
+    private synchronized void spawnTreasure() {
         boolean validPos = false;
         int x = 0, y = 0, points = rand.nextInt(500) + 100;//TODO points for treasure
-        
+
         while (!validPos) {
             x = rand.nextInt(xSize - 1) + 1;
             y = rand.nextInt(xSize - 1) + 1;
-            
+
             if (!boardArray[y][x].equals("w")) {
                 boolean objectFoundAtPos = false;
                 int i = 0;
@@ -103,13 +103,28 @@ public class Game {
         treasures.add(new Treasure(x, y, points));
     }
 
+//    private synchronized void shotFired(PlayerThread player){
+//
+//    }
+//
+//    private synchronized int[] calculateShotFired(int x, int y, String Direction){
+//        int[] a = new int[2];
+//
+//        if(x-1 > 0 &&)
+//
+//
+//        return a;
+//    }
+    
     /**
      *
      * @param message
      * @param player
+     * @throws IOException
      * @throws Exception
      */
-    public synchronized void receiveMessage(String message, PlayerThread player) {
+    public synchronized void receiveMessage(String message, PlayerThread player)
+        throws IOException {
         int x = player.getXpos();
         int y = player.getYpos();
         switch (message.charAt(0)) {
@@ -130,7 +145,36 @@ public class Game {
             break;
         }
         case 'N': {
-            player.setPlayerName(message.substring(1));
+            boolean nameTaken = false;
+            for (PlayerThread p : players) {
+                if (p.getName() != null && p.getName().equals(message.substring(1))) {
+                    nameTaken = true;
+                    break;
+                }
+            }
+            if (nameTaken) {
+                player.sendMessage("0");
+            }
+            else {
+                player.sendMessage("1");
+                player.setPlayerName(message.substring(1));
+                
+                String c = "C";
+                for (PlayerThread p : players) {
+                    if (!p.equals(player)) {
+                        c += p.getPlayerName() + ",";
+                    }
+                }
+                player.sendMessage(c + player.getPlayerName());
+
+                //send navnene på de spillere der allerede er i spillet
+                for (PlayerThread p : players) {
+                    if (!p.equals(player)) {
+                        p.sendMessage("J" + player.getPlayerName());
+                    }
+                }
+                updatePositions();
+            }
             break;
         }
         case 'X': {
@@ -138,13 +182,17 @@ public class Game {
             player.setKeepRunning(false);
             break;
         }
+        case 'S': {
+//            shotFired(player);
+        }
         default: {
             System.out.println("Default i game.receiveMessage");
         }
         }
     }
-
-    private void movePlayer(int x, int y, String direction, PlayerThread player) {
+    
+    private void movePlayer(int x, int y, String direction, PlayerThread player)
+        throws IOException {
         if (boardArray[y][x].equals("w")) {
             player.reducePoints(1);
         }
@@ -157,15 +205,19 @@ public class Game {
             player.increasePoints(1);
         }
         player.setDirection(direction);
-    }
 
+        //U17,6,U,4#13,8,D,0
+        updatePositions();
+    }
+    
     public void removePlayer(PlayerThread player) {
         players.remove(player);
+        notifyAllPlayers("L" + player.getName());
     }
-
+    
     private boolean checkForPlayer(int x, int y, PlayerThread currentPlayer) {
         boolean foundPlayer = false;
-
+        
         for (PlayerThread p : players) {
             if (!p.equals(currentPlayer)) {
                 if (p.getXpos() == x && p.getYpos() == y) {
@@ -178,27 +230,50 @@ public class Game {
         return foundPlayer;
     }
 
-    public synchronized void notifyPlayers() throws IOException {
-        String s = "";
+    private synchronized void updatePositions() {
+        String message = "U";
         for (PlayerThread p : players) {
-            s += p.getPlayerName() + "," + p.getXpos() + "," + p.getYpos() + "," + p.getDirection()
-                + "," + p.getPoint() + "#";
+            message +=
+                p.getXpos() + "," + p.getYpos() + "," + p.getDirection() + "," + p.getPoint() + "#";
         }
-        for (PlayerThread p : players) {
-            p.sendMessage(s.substring(0, s.length() - 1));
-        }
-        //TODO send hvor henne treasures er
+        notifyAllPlayers(message.substring(0, message.length() - 1));
+    }
+    
+    public synchronized void notifyAllPlayers(String message) {
+//        String s = "";
+//        for (PlayerThread p : players) {
+//            s += p.getPlayerName() + "," + p.getXpos() + "," + p.getYpos() + "," + p.getDirection()
+//                + "," + p.getPoint() + "#";
+//        }
 
-        //TODO vi har mulighed for at gøre den sendte besked mindre,
-        /*
-         *Hvis vi kun sender navnet første gang der er en ny spiller, det kan gøres ved at have et array
-         *på hver client der holder på navnene og så for at kende forskel på om det er en normal besked
-         *eller en besked med opdatering i spillere (sker både ved remove og add player)
-         *kan vores beskeder fx være:
-         *XCasper,17,6,U,4#Casper,13,8,D,0 (ny/fjernet spiller)
-         *Z17,6,U,4#13,8,D,0
-         *Derved slipper vi for en hel del data da den ikke skal sende navnene med hele tiden
-         */
+        try {
+            for (PlayerThread p : players) {
+                p.sendMessage(message);
+            }
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
+    //TODO vi har mulighed for at gøre den sendte besked mindre,
+    /*
+     *Hvis vi kun sender navnet første gang der er en ny spiller, det kan gøres ved at have et array
+     *på hver client der holder på navnene og så for at kende forskel på om det er en normal besked
+     *eller en besked med opdatering i spillere (sker både ved remove og add player)
+     *kan vores beskeder fx være:
+     *Derved slipper vi for en hel del data da den ikke skal sende navnene med hele tiden
+     */
+
+    /*
+     *Protokoller:
+     *  LCasper   (sendes når en leaver)
+     *  JCasper,xx,yy,d,p     (sendes når en joiner til dem der allerede er i spillet)
+     *  CCasper,Bob,Mike        (Sendes til en der lige er joinet)
+     *  U17,6,U,4#13,8,D,0    (sendes når nogen går eller point skifter)
+     *  Txx,yy    (treasure spawner)
+     *  Sxx,yy,xx,yy  (laser skud, xy start og xy slut)
+     */
+    
 }
--- src/server/MainAppServer.java
+++ src/server/MainAppServer.java
@@ -4,26 +4,26 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class MainAppServer {
-    
+
     public static void main(String[] args) {
         String board =
-            "wwwwwwwwwwwwwwwwwwwww        ww        ww w  w  www w  w  www w  w   ww w  w  www  w               ww w w w w w w  w  www w     www w  w  www w     w w w  w  www   w w  w  w  w   ww     w  w  w  w   ww ww ww        w  www  w w    w    w  www        ww w  w  www         w w  w  www        w     w  www  w              www  w www  w w  ww www w      ww w     www   w   ww  w      wwwwwwwwwwwwwwwwwwwww";
-
-        Game game = new Game(board);
+            "wwwwwwwwwwwwwwwwwwwww        ww        ww w  w  www w  w  www w  w   ww w  w  www  w               ww w w w w w w  w  www w     www w  w  www w     www w  w  www   w w  w  w  w   ww     w  w  w  w   ww ww ww        w  www  w w    w    w  www        ww w  w  www         w w  w  www        w     w  www  w              www  w www  w w  ww www w      ww w     www   w   ww  w      wwwwwwwwwwwwwwwwwwwww";
         
+        Game game = new Game(board);
+
         try {
             ServerSocket welcomeSocket = new ServerSocket(1337);
             while (true) {
                 Socket connectionSocket = welcomeSocket.accept();
                 PlayerThread player = new PlayerThread(connectionSocket, game, 0, 0, "D");
-                
+
                 player.start();
             }
-            
+
         }
         catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
+
 }
